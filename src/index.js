38687564eae38c76d9df2a9cf201eb9cedcee11f
sikang <updream4@naver.com>
2020-07-29 22:47:18 +0900

    IE 환경 호환 설정

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "./routes/Home.css";
+import "react-app-polyfill/ie11";
+import "react-app-polyfill/stable";
 
 ReactDOM.render(
   <React.StrictMode>
